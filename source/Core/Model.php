683cfea50bf15c86b9c7776f170d872c3c39c0d5
Tiago de Abreu <devtiagoabreu@gmail.com>
2020-07-22 17:10:43 -0300

    refatorando - 09.02 - Abstract model query builder

--- source/Core/Model.php
+++ source/Core/Model.php
@@ -139,6 +139,7 @@ abstract class Model
     public function order(string $columnOrder): Model
     {
         $this->order = " ORDER BY {$columnOrder}";
+        return $this;
     }
 
     /**
@@ -148,6 +149,7 @@ abstract class Model
     public function limit(int $limit): Model
     {
         $this->limit = " LIMIT {$limit}";
+        return $this;
     }
 
     /**
@@ -157,6 +159,7 @@ abstract class Model
     public function offset(int $offset): Model
     {
         $this->offset = " OFFSET {$offset}";
+        return $this;
     }
 
     /**
@@ -247,7 +250,7 @@ abstract class Model
      * @param string $value
      * @return bool
      */
-    protected function delete(string $key, string $value): bool
+    public function delete(string $key, string $value): bool
     {
         try {
             $stmt = Connect::getInstance()->prepare("DELETE FROM " . static::$entity . " WHERE {$key} = :key");
@@ -256,7 +259,7 @@ abstract class Model
             return true;
         } catch (\PDOException $exception) {
             $this->fail = $exception;
-            return null;
+            return false;
         }
     }
 
