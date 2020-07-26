61ccfcc0977aae4c2add534bff70cf68ba29564c
Tiago de Abreu <devtiagoabreu@gmail.com>
2020-07-26 18:20:44 -0300

    IMPORT DataModel AND Note ALTER TABLE posts

--- note/notesSQL.sql
+++ note/notesSQL.sql
@@ -1,2 +1,9 @@
+
+/**
+* Alteração nos campos title e subtitle da tabela posts 
+* para implementar o recurso de busca de posts do blog
+* Busca utilizando a consulta "MATCH AGAINST"
+* Referente ao commit: 10.02 - Registro inicial e FULLTEXT - f9044fa09291018e439d0a31b05a4d146f210724
+*/ 
 ALTER TABLE posts
 ADD FULLTEXT(title,subtitle);
