d4d359a247a947c393e39d0677d0365adfd8befe
Tiago de Abreu <devtiagoabreu@gmail.com>
2020-09-07 21:36:02 -0300

    11.9 - Lançamento de contas

--- source/App/App.php
+++ source/App/App.php
@@ -61,7 +61,7 @@ class App extends Controller
         $chartData->income = "0,0,0,0,0";
 
         $chart = (new AppInvoice())
-            ->find("user_id = :user AND status = :status AND due_at >= DATE(NOW() - INTERVAL 3 MONTH) GROUP BY year(due_at) ASC, month(due_at) ASC",
+            ->find("user_id = :user AND status = :status AND due_at BETWEEN DATE(NOW() - INTERVAL 3 MONTH) AND DATE(NOW() + INTERVAL 2 MONTH) GROUP BY year(due_at) ASC, month(due_at) ASC",
                 "user={$this->user->id}&status=paid",
                  "
                     year(due_at) AS due_year,
@@ -91,13 +91,13 @@ class App extends Controller
 
         //INCOME && EXPENSE
         $income = (new AppInvoice())
-            ->find("user_id = :user AND type = 'income' AND status = 'unpaid' AND date(due_at) <= DATE(NOW() + INTERVAL 1 MONTH)",
+            ->find("user_id = :user AND type = 'income' AND status = 'unpaid' AND date(due_at) BETWEEN DATE(NOW() - INTERVAL 3 MONTH) AND DATE(NOW() + INTERVAL 2 MONTH)",
                 "user={$this->user->id}")
             ->order("due_at")
             ->fetch(true);
 
         $expense = (new AppInvoice())
-        ->find("user_id = :user AND type = 'expense' AND status = 'unpaid' AND date(due_at) <= DATE(NOW() + INTERVAL 1 MONTH)",
+        ->find("user_id = :user AND type = 'expense' AND status = 'unpaid' AND date(due_at) BETWEEN DATE(NOW() - INTERVAL 3 MONTH) AND DATE(NOW() + INTERVAL 2 MONTH)",
                 "user={$this->user->id}")
         ->order("due_at")
         ->fetch(true);
@@ -163,6 +163,70 @@ class App extends Controller
         echo $this->view->render("expense", [
             "head" => $head
         ]);
+    }
+
+    public function launch(array $data): void
+    {
+        if (request_limit("applaunch", 20, 60 * 5)) {
+            $json["message"] = $this->message->warning("Foi muito rápido {$this->user->first_name}! Por favor aguarde 5 minutos para novos lançamentos.")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        if (!empty($data["enrollments"]) && ($data["enrollments"] < 2 || $data["enrollments"] > 420)) {
+            $json["message"] = $this->message->warning("Ooops {$this->user->first_name}! O número de parcelas deve ser entre 2 e 420.")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $data = filter_var_array($data, FILTER_SANITIZE_STRIPPED);
+        $status = (date($data["due_at"]) <= date("Y-m-d") ? "paid" : "unpaid");
+
+        $invoice = (new AppInvoice());
+        $invoice->user_id = $this->user->id;
+        $invoice->wallet_id = $data["wallet"];
+        $invoice->category_id = $data["category"];
+        $invoice->invoice_of = null;
+        $invoice->description = $data["description"];
+        $invoice->type = ($data["repeat_when"] == "fixed" ? "fixed_{$data["type"]}" : $data["type"]);
+        $invoice->value = str_replace([".", ","], ["","."], $data["value"]);
+        $invoice->currency = $data["currency"];
+        $invoice->due_at = $data["due_at"];
+        $invoice->repeat_when = $data["repeat_when"];
+        $invoice->period = ($data["period"] ?? "month");
+        if (!$data["enrollments"] || $data["enrollments"] == ''){
+            $invoice->enrollments = 0;
+        }
+        $invoice->enrollment_of = 1;
+        $invoice->status = ($data["repeat_when"] == "fixed" ? "paid" : $status);
+        
+        if (!$invoice->save()) {
+            var_dump($invoice);
+            $json["message"] = $invoice->message()->before("Ooops! ")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        if ($invoice->repeat_when == "enrollment") {
+            $invoiceOf = $invoice->id;
+            for ($enrollment = 1; $enrollment < $invoice->enrollments; $enrollment++) {
+                $invoice->id = null;
+                $invoice->invoice_of = $invoiceOf;
+                $invoice->due_at = date("Y-m-d", strtotime($data["due_at"] . "+{$enrollment}month"));
+                $invoice->status = (date($invoice->due_at) <= date("Y-m-d") ? "paid" : "unpaid");
+                $invoice->enrollment_of = $enrollment + 1;
+                $invoice->save();
+            }
+        }
+
+        if ($invoice->type == "income") {
+            $this->message->success("Receita lançada com sucesso. Use o filtro para controlar.")->render();
+        } else {
+            $this->message->success("Despesa lançada com sucesso. Use o filtro para controlar.")->render();
+        }
+
+        $json["reload"] = true;
+        echo json_encode($json);
     }
 
     /**
